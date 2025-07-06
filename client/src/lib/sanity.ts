63cdfa258ae2f48b11edc28ee2dd306bab7203c9
gosouza_meli <govinda.souza@mercadolivre.com>
2025-07-06 14:49:41 -0300

    up

--- client/src/lib/sanity.ts
+++ client/src/lib/sanity.ts
@@ -2,7 +2,7 @@ import { createClient } from '@sanity/client'
 import imageUrlBuilder from '@sanity/image-url'
 
 export const client = createClient({
-  projectId: import.meta.env.VITE_SANITY_PROJECT_ID || 'your-project-id',
+  projectId: import.meta.env.VITE_SANITY_PROJECT_ID || '4y88u6cf',
   dataset: import.meta.env.VITE_SANITY_DATASET || 'production',
   useCdn: true,
   apiVersion: '2023-05-03',
