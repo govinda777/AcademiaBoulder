8c102f90762736c42dfc2409ac4882c212743f39
Govinda <govinda777@gmail.com>
2026-02-03 10:32:49 -0300

    feat: criar índice de exportação para componentes SEO

---
+++ client/src/components/seo/index.ts
@@ -0,0 +1,3 @@
+export { SEOHead } from './SEOHead';
+export { StructuredData, createLocalBusinessSchema, createCourseSchema, createEventSchema, createBreadcrumbSchema } from './StructuredData';
+export type { SEOHeadProps } from './SEOHead';
